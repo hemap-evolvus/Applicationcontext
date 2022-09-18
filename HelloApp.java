9b9da8e20a731990504ce5c60844e8ac0e3c4229
hemap-evolvus <113161119+hemap-evolvus@users.noreply.github.com>
2022-09-18 15:36:32 +0530

    Add files via upload

---
+++ HelloApp.java
@@ -0,0 +1,13 @@
+package com.spring.application.context;
+
+public class HelloApp {
+
+	 private String message;
+
+	   public void setMessage(String message){
+	      this.message  = message;
+	   }
+	   public void getMessage(){
+	      System.out.println("Your Message : " + message);
+	   }
+	}
